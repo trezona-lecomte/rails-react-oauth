cdc296ae8426ba7c74f79acfce4f9cfd4a7dac9a
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-07-26 02:09:02 +1200

    Export BlabsView component which renders BlabsList

---
+++ client/components/blabs/View.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var BlabsList = require('./List.jsx');
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.setState({data: [{id: 2, content: 'and another fake blab'}, {id: 1, content: 'this is a fake blab'}]});
+    },
+    render: function() {
+        return (
+            <div className="blabs-view">
+              <BlabsList data={this.state.data} />
+            </div>
+        );
+    }
+});
+
+// A React component is literally just a function of it's state and
+// it's props. State should contain dynamic values that impact the UI,
+// and a component is responsible for managing it's own state. Props
+// are usually received from upstream and may contain data,
+// configuration, or functionality, and they are immutable as far as
+// the component receiving them is concerned.
+
+// In this case, we have added "data" to BlabView's state, and
+// subsequently assigned the value of that state to a prop of
+// BlabsList, so we have restricted the responsibilty for mutating that
+// piece of state to BlabView, but BlabList will still receive any
+// changes in that state as this.props.data. This pattern is described
+// in the docs:
+
+// A common pattern is to create several stateless components that
+// just render data, and have a stateful component above them in the
+// hierarchy that passes its state to its children via props. The
+// stateful component encapsulates all of the interaction logic, while
+// the stateless components take care of rendering data declaratively.
