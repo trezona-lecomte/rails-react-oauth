013c2656f71868cdcd06b3179007f0448b96a781
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-07-26 15:38:00 +1200

    Implement writeBlabToAPI and pass down to BlabsForm

--- client/components/blabs/View.jsx
+++ client/components/blabs/View.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var BlabsList = require('./List.jsx');
+var BlabsForm = require('./Form.jsx');
 
 module.exports = React.createClass({
     getInitialState: function() {
@@ -13,9 +14,17 @@ module.exports = React.createClass({
             this.setState({data: blabs});
         }.bind(this));
     },
+    writeBlabToAPI: function(data) {
+        this.props.writeToAPI('post', this.props.origin + '/blabs', data, function(blab) {
+            var blabs = this.state.data;
+            blabs.unshift(blab);
+            this.setState({data: blabs});
+        }.bind(this));
+    },
     render: function() {
         return (
             <div className="blabs-view">
+              <BlabsForm writeBlabToAPI={this.writeBlabToAPI} signedIn={this.props.signedIn} />
               <BlabsList data={this.state.data} />
             </div>
         );
