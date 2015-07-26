3975afe7a19b4159a67db7aa29d8c3c8c4d3c602
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-07-26 13:21:49 +1200

    Meaningless static update to About view

--- client/components/static/AboutView.jsx
+++ client/components/static/AboutView.jsx
@@ -5,11 +5,7 @@ module.exports = React.createClass({
         return (
             <div id="about-view">
               <h1>About</h1>
-              <p>
-                An example of a stateless Ruby API using the rails-api gem, with a React client-side app.
-                To fit the definition of stateless, the API does not include action-view, sprockets, or sessions.
-                Roughly speaking, React replaces action-view, Webpack replaces sprockets, and JWT replaces sessions.
-              </p>
+              <p>An example of a stateless Ruby API using the rails-api gem, with a React client side app. To fit the definition of stateless, the API does not include action-view, sprockets, or sessions. Roughly speaking, React replaces action-view, Webpack replaces sprockets, and JWT replaces sessions.</p>
             </div>
         );
     }
