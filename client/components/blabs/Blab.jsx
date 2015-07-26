da6378e0fe30692aeeaef7b802f12cf2058038d6
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-07-26 15:34:30 +1200

    Pass author down from BlabList to Blab

--- client/components/blabs/Blab.jsx
+++ client/components/blabs/Blab.jsx
@@ -4,6 +4,7 @@ module.exports = React.createClass({
     render: function() {
         return (
             <li className="blab">
+              <span className="blab-author">{this.props.author}: </span>
               <span className="blab-text">{this.props.content}</span>
             </li>
         );
