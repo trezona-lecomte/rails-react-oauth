71edbeb044cbb8eef0d3badb62163cc166cb73e3
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-07-26 12:47:57 +1200

    Fix bug with instantiating Router.Link

--- client/components/layout/Menu.jsx
+++ client/components/layout/Menu.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var Router = require('react-router');
-var Link = Router.link;
+var Link = Router.Link;
 
 module.exports = React.createClass({
     render: function() {
